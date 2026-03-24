62ef125cd77713469f094ce25093d4db5eac451f
Arushi <arushiluthra1604@gmail.com>
2026-03-25 00:18:30 +0530

    addition

---
+++ server/db/sequelize.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import { config } from "../config.js";
+
+export const sequelize = new Sequelize(
+  config.db.name,
+  config.db.user,
+  config.db.password,
+  {
+    host: config.db.host,
+    port: config.db.port,
+    dialect: "mysql",
+    logging: false,
+    define: {
+      underscored: true,
+    },
+  },
+);
